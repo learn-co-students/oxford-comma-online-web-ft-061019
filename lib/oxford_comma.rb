bae065526fb77b7e5e53353568af7cf3d59b5600
FreeBreadsticks <erika.hughes@dbspublishing.com>
2019-06-14 17:51:46 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  end
+  if array.size == 2
+   last = array.pop
+   return array.join(", ") + " and #{last}"
+  end 
+  last = array.pop
+  array.join(", ") + ", and #{last}"
 end
